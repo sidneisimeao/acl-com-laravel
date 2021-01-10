67155bb8cadb2fb8596f7c0acfe1ac3cb09d96eb
Sidnei Simmon <sidneisimmon@gmail.com>
2021-01-10 14:12:04 -0300

    mapeamento de dados

--- app/Reply.php
+++ app/Reply.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Reply extends Model
 {
-    //
+    protected $fillable = ['replay'];
+
+    public function thread()
+    {
+        return $this->belongsTo(Thread::class);
+    }
 }
--- app/Thread.php
+++ app/Thread.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Thread extends Model
 {
-    //
+    protected $fillable = ['title', 'body'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function replies()
+    {
+        return $this->hasMany(Reply::class);
+    }
 }
